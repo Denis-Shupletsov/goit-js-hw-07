6bdf80cecffe88371cac06283be2b75411c3e236
Denis-Shupletsov <b128k2351763@gmail.com>
2023-03-18 16:59:45 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createItemsMarkup(item) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+                class="gallery__image"
+                src="small-image.jpg"
+                data-source="large-image.jpg"
+                alt="Image description"
+            />
+        </a>
+</li>`;
+    }).join('');
+
+}
 console.log(galleryItems);
