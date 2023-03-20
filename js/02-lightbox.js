c2b6e07a4496c59a651db1b05b454da82896f194
Denis-Shupletsov <b128k2351763@gmail.com>
2023-03-20 09:16:00 +0200

    2ok

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createItemsMarkup(item) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<ul class="gallery">
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+            />
+        </a></li>
+</ul>`;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionType: 'alt'
+});
